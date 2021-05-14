7e7701f3e894d90f6cf0159cd4f789c3e2ad27da
PhanThanhTrung <trung99nb@gmail.com>
2021-05-14 09:15:36 +0700

    formatted code

--- evaluate.py
+++ evaluate.py
@@ -20,21 +20,24 @@ random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 
-def tokenize_and_cut(sentence: str, max_input_length:int= 256):
-    tokens = tokenizer.tokenize(sentence) 
+
+def tokenize_and_cut(sentence: str, max_input_length: int = 256):
+    tokens = tokenizer.tokenize(sentence)
     tokens = tokens[:max_input_length-2]
     return tokens
+
 
 def binary_accuracy(preds, y):
     """
         Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
     """
 
-    #round predictions to the closest integer
-    rounded_preds = torch.argmax(torch.softmax(preds, dim = 1), dim = 1)
-    correct = (rounded_preds == y).float() #convert into float for division 
+    # round predictions to the closest integer
+    rounded_preds = torch.argmax(torch.softmax(preds, dim=1), dim=1)
+    correct = (rounded_preds == y).float()  # convert into float for division
     acc = correct.sum() / len(correct)
     return acc
+
 
 if __name__ == '__main__':
     hidden_dim = 256
@@ -47,7 +50,7 @@ if __name__ == '__main__':
     phobert_path = "vinai/phobert-base"
     source_folder = '/root/dataset/sentiment_analysis/'
     batch_size = 8
-    
+
     tokenizer = AutoTokenizer.from_pretrained(phobert_path, use_fast=False)
     init_token = tokenizer.cls_token
     eos_token = tokenizer.sep_token
@@ -57,29 +60,27 @@ if __name__ == '__main__':
     eos_token_idx = tokenizer.convert_tokens_to_ids(eos_token)
     pad_token_idx = tokenizer.convert_tokens_to_ids(pad_token)
     unk_token_idx = tokenizer.convert_tokens_to_ids(unk_token)
-    
-    TEXT = Field(batch_first = True,
-                  use_vocab = False,
-                  tokenize = tokenize_and_cut,
-                  preprocessing = tokenizer.convert_tokens_to_ids,
-                  init_token = init_token_idx,
-                  eos_token = eos_token_idx,
-                  pad_token = pad_token_idx,
-                  unk_token = unk_token_idx)
-    LABEL = LabelField(dtype = torch.long, use_vocab =False)
+
+    TEXT = Field(batch_first=True,
+                 use_vocab=False,
+                 tokenize=tokenize_and_cut,
+                 preprocessing=tokenizer.convert_tokens_to_ids,
+                 init_token=init_token_idx,
+                 eos_token=eos_token_idx,
+                 pad_token=pad_token_idx,
+                 unk_token=unk_token_idx)
+    LABEL = LabelField(dtype=torch.long, use_vocab=False)
     fields = [('data', TEXT), ('label', LABEL)]
     train, valid, test = TabularDataset.splits(path=source_folder, train='train.csv', validation='validation.csv', test='test.csv',
-                                           format='CSV', fields=fields, skip_header=True)
-    
+                                               format='CSV', fields=fields, skip_header=True)
 
     train_generator, val_generator, test_generator = BucketIterator.splits(
-            (train, valid, test), 
-            batch_size = batch_size, 
-            device = device, sort = False)
-    
-    
+        (train, valid, test),
+        batch_size=batch_size,
+        device=device, sort=False)
+
     criterion = nn.CrossEntropyLoss()
-    
+
     criterion = criterion.to(device)
     all_statedict_path = glob.glob('/root/logs/*.pth')
     for state_dict_path in all_statedict_path:
@@ -87,16 +88,16 @@ if __name__ == '__main__':
         epoch_loss = 0
         epoch_acc = 0
         model = phobert_lstm(phobert_path=phobert_path,
-                             state_dict_path = state_dict_path,
-                             hidden_dim = hidden_dim,
-                             num_classes = num_classes,
-                             n_layers= n_layers,
-                             bidirectional= bidirectional,
-                             dropout = dropout,
-                             device = device)
+                             state_dict_path=state_dict_path,
+                             hidden_dim=hidden_dim,
+                             num_classes=num_classes,
+                             n_layers=n_layers,
+                             bidirectional=bidirectional,
+                             dropout=dropout,
+                             device=device)
         model.eval()
         with torch.no_grad():
-            for batch in tqdm.tqdm(test_generator, desc = "Evaluating"):
+            for batch in tqdm.tqdm(test_generator, desc="Evaluating"):
                 predictions = model(batch.data).squeeze(1)
                 loss = criterion(predictions, batch.label)
                 acc = binary_accuracy(predictions, batch.label)
@@ -105,5 +106,3 @@ if __name__ == '__main__':
                 epoch_acc += acc.item()
         print('Evaluating Accuracy ', epoch_acc/len(test_generator))
         print('Evaluating Loss ', epoch_loss/len(test_generator))
-    
-
--- train_lstm_phobert.py
+++ train_lstm_phobert.py
@@ -28,20 +28,23 @@ random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
 
-def tokenize_and_cut(sentence: str, max_input_length:int= 256):
-    tokens = tokenizer.tokenize(sentence) 
+
+def tokenize_and_cut(sentence: str, max_input_length: int = 256):
+    tokens = tokenizer.tokenize(sentence)
     tokens = tokens[:max_input_length-2]
     return tokens
+
 
 def binary_accuracy(preds, y):
     """
         Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
     """
-    #round predictions to the closest integer
-    rounded_preds = torch.argmax(torch.softmax(preds, dim = 1), dim = 1)
-    correct = (rounded_preds == y).float() #convert into float for division 
+    # round predictions to the closest integer
+    rounded_preds = torch.argmax(torch.softmax(preds, dim=1), dim=1)
+    correct = (rounded_preds == y).float()  # convert into float for division
     acc = correct.sum() / len(correct)
     return acc
+
 
 if __name__ == '__main__':
     hidden_dim = 256
@@ -65,8 +68,9 @@ if __name__ == '__main__':
                          bidirectional,
                          dropout,
                          device)
-    
-    tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base", use_fast=False)
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        "vinai/phobert-base", use_fast=False)
     init_token = tokenizer.cls_token
     eos_token = tokenizer.sep_token
     pad_token = tokenizer.pad_token
@@ -76,30 +80,28 @@ if __name__ == '__main__':
     pad_token_idx = tokenizer.convert_tokens_to_ids(pad_token)
     unk_token_idx = tokenizer.convert_tokens_to_ids(unk_token)
     max_input_length = tokenizer.max_model_input_sizes['vinai/phobert-base']
-    TEXT = Field(batch_first = True,
-                  use_vocab = False,
-                  tokenize = tokenize_and_cut,
-                  preprocessing = tokenizer.convert_tokens_to_ids,
-                  init_token = init_token_idx,
-                  eos_token = eos_token_idx,
-                  pad_token = pad_token_idx,
-                  unk_token = unk_token_idx)
-    LABEL = LabelField(dtype = torch.long, use_vocab =False)
+    TEXT = Field(batch_first=True,
+                 use_vocab=False,
+                 tokenize=tokenize_and_cut,
+                 preprocessing=tokenizer.convert_tokens_to_ids,
+                 init_token=init_token_idx,
+                 eos_token=eos_token_idx,
+                 pad_token=pad_token_idx,
+                 unk_token=unk_token_idx)
+    LABEL = LabelField(dtype=torch.long, use_vocab=False)
     fields = [('data', TEXT), ('label', LABEL)]
     train, valid, test = TabularDataset.splits(path=SOURCE_FOLDER, train='train.csv', validation='validation.csv', test='test.csv',
-                                           format='CSV', fields=fields, skip_header=True)
-    
+                                               format='CSV', fields=fields, skip_header=True)
 
     train_generator, val_generator, test_generator = BucketIterator.splits(
-            (train, valid, test), 
-            batch_size = BATCH_SIZE, 
-            device = device, sort = False)
-    
-    
+        (train, valid, test),
+        batch_size=BATCH_SIZE,
+        device=device, sort=False)
+
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
     writer = tensorboardX.SummaryWriter()
-    optimizer = optim.Adam(model.parameters(), lr = 1e-4)
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
     criterion = nn.CrossEntropyLoss()
     criterion = criterion.to(device)
     global_count = 0
@@ -108,7 +110,7 @@ if __name__ == '__main__':
         epoch_loss = 0
         epoch_acc = 0
         model.train()
-        for batch in tqdm.tqdm(train_generator, desc = 'Training'):
+        for batch in tqdm.tqdm(train_generator, desc='Training'):
             optimizer.zero_grad()
             predictions = model(batch.data).squeeze(1)
             loss = criterion(predictions, batch.label)
@@ -118,29 +120,32 @@ if __name__ == '__main__':
             epoch_loss += loss.item()
             epoch_acc += acc.item()
 
-            
-        writer.add_scalar('Training_Accuracy', epoch_acc/len(train_generator), global_count)
-        writer.add_scalar('Training_Loss', epoch_loss/len(train_generator), global_count)
+        writer.add_scalar('Training_Accuracy', epoch_acc /
+                          len(train_generator), global_count)
+        writer.add_scalar('Training_Loss', epoch_loss /
+                          len(train_generator), global_count)
         epoch_loss = 0
         epoch_acc = 0
-        
+
         model.eval()
         with torch.no_grad():
-            for batch in tqdm.tqdm(val_generator, desc = "Validation"):
+            for batch in tqdm.tqdm(val_generator, desc="Validation"):
                 predictions = model(batch.data).squeeze(1)
-                
+
                 loss = criterion(predictions, batch.label)
-                
+
                 acc = binary_accuracy(predictions, batch.label)
 
                 epoch_loss += loss.item()
                 epoch_acc += acc.item()
-        writer.add_scalar('Validation_Accuracy', epoch_acc/len(val_generator), global_count)
-        writer.add_scalar('Validation_Loss', epoch_loss/len(val_generator), global_count)
-        
+        writer.add_scalar('Validation_Accuracy', epoch_acc /
+                          len(val_generator), global_count)
+        writer.add_scalar('Validation_Loss', epoch_loss /
+                          len(val_generator), global_count)
 
-        name = '{}_{}_{:.2f}.pth'.format(epoch, global_count, epoch_loss/len(val_generator))
+        name = '{}_{}_{:.2f}.pth'.format(
+            epoch, global_count, epoch_loss/len(val_generator))
         path_model_state_dict = os.path.join(
             log_dir, name)
         torch.save(model.state_dict(), path_model_state_dict)
-        global_count+=1
+        global_count += 1
